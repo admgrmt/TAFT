95c5a0e3a32e3672fcfe75ec2636829b912e99b7
admgrmt <60199675+admgrmt@users.noreply.github.com>
2020-12-17 21:59:32 -0800

    Add files via upload

---
+++ Untitled(1).py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# rgtrttr
+
+# Is this working
+
+# 
+
+# Mabye it mis... 
+
+# Note sure
+
+# In[ ]:
+
+
+#Going to add to this and see how it functions
+
